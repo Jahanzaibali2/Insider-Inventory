d3c5f3f5a37f815c3581d18f5746056b719a2c74
Jahanzaibali2 <jahanzaibalikhawaja26@gmail.com>
2024-08-26 20:38:13 +0500

    Working On Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from './Components/NavBar/Header'
 import TextAndLabel from './Components/Body/TextAndLabels/TextAndLabel'
+import Footer from './Components/Footer/footer'
 import "./App.css"
 
 export default function App() {
@@ -8,6 +9,7 @@ export default function App() {
     <>
     <Header />
     <TextAndLabel/>
+    <Footer/>
     </>
     )
 }
---
+++ src/Components/Footer/footer.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import FooterLogo from "../../assets/Footerlogo.png"
+import "./footer.css"
+
+export default function Footer() {
+    return (
+        <div>
+            <WholeFooter />
+        </div>
+    )
+}
+
+function WholeFooter() {
+    return (
+        <div>
+            <div className="ft-div">
+
+                <div className="Logo">
+                    <img src={FooterLogo} alt="x" />
+                </div>
+                <div className="listAndSocials">
+                    <div className="ft-list">
+                        <ul>
+                            <li>Home</li>
+                            <li>Buyers</li>
+                            <li>Sellers</li>
+                            <li>Buy & hold</li>
+                            <li>Retail</li>
+                            <li>Flip Opportunities</li>
+                            <li>Off-Market Inventory</li>
+                        </ul>
+                    </div>
+                    <div className="ft-socials">
+                        <button>whatsapp</button>
+                        <button>Twitter</button>
+                        <button>LinkedIn</button>
+                        <button>Instagram</button>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
