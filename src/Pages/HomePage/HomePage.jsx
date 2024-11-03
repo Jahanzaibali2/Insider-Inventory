d2fde1659efa804df82cc2fd5de971d8c8557bc1
Jahanzaibali2 <jahanzaibalikhawaja26@gmail.com>
2024-11-03 19:08:36 +0500

    xa

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -42,7 +42,6 @@ export default function HomePage() {
                                     background: "transparent",
                                     border: "white 1px solid",
                                     color: "var(--green)"
-
                                 }}
 
                             />
